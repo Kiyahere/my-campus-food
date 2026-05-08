95355547b1eb51807be07bd57fbc7d9f456a4f56
KiyahEreijuweimini <kiyahere5@gmail.com>
2026-05-08 12:35:31 -0700

    Removed extra s

--- app.py
+++ app.py
@@ -418,7 +418,7 @@ def add_food():
     filename = file.filename
     file_bytes = file.read()
 
-    supabase.storage.from_("food-images").upload(filename, file.read(), {"content_type": file.content_type})s
+    supabase.storage.from_("food-images").upload(filename, file.read(), {"content_type": file.content_type})
 
     supabase.storage.from_("food-images").upload(filename, file_bytes)
     image_url = supabase.storage.from_("food-images").get_public_url(filename)
@@ -429,7 +429,7 @@ def add_food():
         "image": image_url
     }).execute()
 
-    return redirect("/sdmin_dashboard")
+    return redirect("/admin_dashboard")
 
     conn = sqlite3.connect("foods.db")
     cursor = conn.cursor()
