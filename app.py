fc332f49b93dcd18ce5eb5332f09474a29090185
KiyahEreijuweimini <kiyahere5@gmail.com>
2026-05-14 08:25:26 -0700

    Fix forgot password indentation error

--- app.py
+++ app.py
@@ -266,7 +266,7 @@ def login():
     return render_template("login.html")
 
 
-    @app.route("/forgot-password", methods=["GET", "POST"])
+@app.route("/forgot-password", methods=["GET", "POST"])
 def forgot_password():
 
     if request.method == "POST":
